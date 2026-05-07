5e2c8176cdb9483dfa627372f4bb816bcb1ddcdc
Arthur <151936565+aduarte98@users.noreply.github.com>
2026-05-07 16:30:56 -0300

    Delete keep-alive.js

--- keep-alive.js
+++
@@ -1,1 +0,0 @@
-export default async function handler(req, res) { try { const response = await fetch( "https://sdbobbmqudrhscotfnok.supabase.co/functions/v1/ping" ); const data = await response.text(); res.status(200).json({ ok: true, status: response.status, data }); } catch (error) { res.status(500).json({ ok: false, error: error.message }); } }
